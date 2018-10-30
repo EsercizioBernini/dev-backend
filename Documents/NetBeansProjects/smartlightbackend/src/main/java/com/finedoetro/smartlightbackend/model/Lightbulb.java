90e6c7c83d33ffbeee5aac8bc4c28bbecfe15dc7
aless <alessandro.cerutti96@gmail.com>
2018-10-30 14:09:25 +0100

    base progetto

---
+++ Documents/NetBeansProjects/smartlightbackend/src/main/java/com/finedoetro/smartlightbackend/model/Lightbulb.java
@@ -0,0 +1,38 @@
+package com.finedoetro.smartlightbackend.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author aless
+ */
+@XmlRootElement
+public class Lightbulb {
+
+    private String status;
+    private String message;
+
+    public Lightbulb(String status) {
+        this.status = status;
+    }
+
+    public Lightbulb() {
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
